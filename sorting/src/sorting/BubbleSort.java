a8de83c183ee430c68f4289d3845d2817011aeed
pradeep <pradeep@pradeep-PC>
2017-08-26 10:23:23 +0530

    kkkklk

---
+++ sorting/src/sorting/BubbleSort.java
@@ -0,0 +1,33 @@
+package sorting;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+
+		bubblesort(new int[]{1,2,3,4},3);
+		System.out.println("gjykjkjh");
+		System.out.println("gjykjkjh");
+		System.out.println("gjykjkjh");
+		System.out.println("gjykjkjh");
+
+	}
+
+	
+	static void bubblesort(int a[],int n)
+	{
+		int i,j,temp;
+		for( i=0; i<n;i++)
+		{
+			for(j=0;j<n-i;j++)
+			{
+				if(a[j]>a[j+1])
+				{
+					temp=a[j];
+					a[j]= a[j+1];
+					a[j+1]=temp;
+							
+				}
+			}
+		}
+		}
+}
